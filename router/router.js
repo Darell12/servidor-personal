1b75219f4f4474918c7f9ffe6cbe4fb46bc584e1
Real_Darell <79779792+Darell12@users.noreply.github.com>
2022-09-20 08:31:24 -0500

    Update router.js

--- router/router.js
+++ router/router.js
@@ -3,23 +3,8 @@ const router = express.Router();
 
 
 import { 
-    home,
-    operacion,
-    saludo,
     calcular
 } from '../controller/index.js';
-
-router
-    .route('/home')
-    .get(home)
-
-router
-    .route('/operacion/:numero') // Recibir parametros
-    .get(operacion)
-
-router
-    .route('/saludo/:nombre') // Recibir parametros
-    .get(saludo)
 
 router
     .route('/calcular/:result')
