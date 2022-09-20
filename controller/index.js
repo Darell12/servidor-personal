09e135fedd85ee93949d851861b1158b07ec5649
Real_Darell <79779792+Darell12@users.noreply.github.com>
2022-09-20 08:30:45 -0500

    Update index.js

--- controller/index.js
+++ controller/index.js
@@ -1,33 +1,4 @@
-
-// Middleware
-const home = (req, res) =>{ 
-    res.send("Bienvenidos Estudiantes SENA - Primer Servidor ExpressJS");
-};
-
-
-// Middleware que multiplica el numero recibido por 5
-const operacion =   (req, res) =>{ 
-    const { numero } = req.params;
-    
-    let resultado = numero * 9; 
-    
-    res.json({
-        "resultado" : resultado 
-    });
-
-};
-
-// Middleware que multiplica el numero recibido por 5
-const saludo =   (req, res) =>{ 
-    const { nombre } = req.params; 
-    
-    res.json({
-        "saludo" : "Bienvenido Sr(a)",
-        "nombre" : nombre
-    });
-};
-
-
+//Middleware
 const calcular = (req, res) =>{
     const {result} = req.params;
 
@@ -40,8 +11,5 @@ const calcular = (req, res) =>{
 }
 
 export {
-    home,
-    operacion,
-    saludo,
     calcular
 }
